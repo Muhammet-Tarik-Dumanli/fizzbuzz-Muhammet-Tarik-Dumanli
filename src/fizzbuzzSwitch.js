e091a0d8a6a1955b4425df48d6a6b9cf3f6e6371
Muhammet-Tarik-Dumanli <mtdumanli@gmail.com>
2025-05-15 09:33:33 +0300

    tests passed

--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,18 @@
 function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+  switch (true) {
+    case num % 3 === 0 && num % 5 === 0:
+      return "FizzBuzz";
+      break;
+    case num % 3 === 0:
+      return "Fizz";
+      break;
+    case num % 5 === 0:
+      return "Buzz";
+      break;
+    default:
+      return num;
+      break;
+  }
 }
 
 module.exports = fizzbuzzSwitch;
